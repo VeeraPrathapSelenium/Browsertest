6e6e75480d722594f19471a1899b741488b2b4d7
veera <prathap.ufttest@gmail.com>
2017-06-03 20:41:33 +0530

    my jenkins test

---
+++ src/test/java/com/Mytest/ClassA.java
@@ -0,0 +1,18 @@
+package com.Mytest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ClassA {
+	
+	public static WebDriver driver;
+	
+	@Test
+	public static void startBrowser()
+	{
+		driver=new FirefoxDriver();
+		driver.navigate().to("https://www.facebook.com/");
+		
+	}
+
+}
